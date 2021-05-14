69d59e3b8286150ae4c120a2ca86b4879e1ce00e
TheRedHotHabanero <lkrn10001@gmail.com>
2021-05-14 19:28:56 +0300

    bad menu made

--- menu.cpp
+++ menu.cpp
@@ -2,3 +2,129 @@
 // Created by karina on 12.05.2021.
 //
 
+#include "menu.h"
+
+void Menu::create_menu_background(const string &menu_background_)
+{
+    menu_background.loadFromFile(menu_background_);
+    menu_background_texture.loadFromImage(menu_background);
+    menu_background_sprite.setTexture(menu_background_texture);
+    menu_background_sprite.setPosition(WINDOW_POSITION_X, WINDOW_POSITION_Y);
+}
+
+void Menu::create_exit_button(const string &exit_button_)
+{
+    exit_button.loadFromFile(exit_button_);
+    exit_button_texture.loadFromImage(exit_button);
+    exit_button_sprite.setTexture(exit_button_texture);
+    exit_button_sprite.setPosition(EXIT_BUTTON_POS_X, EXIT_BUTTON_POS_Y);
+}
+
+void Menu::create_start_info_button(const string &start_info_button_)
+{
+    start_info_button.loadFromFile(start_info_button_);
+    start_info_button_texture.loadFromImage(start_info_button);
+    start_info_button_sprite.setTexture(start_info_button_texture);
+    start_info_button_sprite.setPosition(START_INFO_BUTTON_POS_X, START_INFO_BUTTON_POS_Y);
+}
+
+void Menu::create_checking_odds_button(const string &checking_odds_button_)
+{
+    checking_odds_button.loadFromFile(checking_odds_button_);
+    checking_odds_texture.loadFromImage(checking_odds_button);
+    checking_odds_sprite.setTexture(checking_odds_texture);
+    checking_odds_sprite.setPosition(CHECKING_ODDS_BUTTON_POS_X, CHECKING_ODDS_BUTTON_POS_Y);
+
+}
+void Menu::create_about_button(const string &about_button_)
+{
+    about_button.loadFromFile(about_button_);
+    about_button_texture.loadFromImage(about_button);
+    about_button_sprite.setTexture(about_button_texture);
+    about_button_sprite.setPosition(ABOUT_BUTTON_POS_X, ABOUT_BUTTON_POS_Y);
+}
+
+void Menu::welcome_page(const string& menu_background_,
+                        const string& start_info_button_,
+                        const string& exit_button_,
+                        const string& about_button,
+                        const string& checking_odds_button_)
+{
+    RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT),
+                        "МФТИ (Московский физкультурный техникум для идиотов) ");
+
+    create_menu_background(menu_background_);
+    create_exit_button(exit_button_);
+    create_start_info_button(start_info_button_);
+    create_checking_odds_button(checking_odds_button_);
+    create_about_button(about_button);
+
+    window.clear();
+
+    window.draw(menu_background_sprite);
+    window.draw(exit_button_sprite);
+    window.draw(start_info_button_sprite);
+    window.draw(about_button_sprite);
+    window.draw(checking_odds_sprite);
+
+    window.display();
+    processing_menu(window);
+}
+
+void Menu::start_info_button_pressed(RenderWindow &window)
+{
+    std::cout<< "start_info_button" << std::endl;
+}
+
+void Menu::about_button_pressed(RenderWindow &window)
+{
+    std::cout<< "about_button" << std::endl;
+}
+
+void Menu::checking_odds_button_pressed(RenderWindow &window)
+{
+    std::cout << "checking_odds_pressed" << std::endl;
+}
+
+void Menu::exit_button_pressed(RenderWindow& window)
+{ window.close(); }
+
+void Menu::processing_menu(RenderWindow& window)
+{
+    while (window.isOpen())
+    {
+        Event event;
+
+        if (event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape))
+            exit_button_pressed(window);
+
+        if (IntRect(START_INFO_BUTTON_POS_X, START_INFO_BUTTON_POS_Y,
+                    START_INFO_BUTTON_WIDTH, START_INFO_BUTTON_HEIGHT).contains(Mouse::getPosition(window)))
+            menuNum = START_INFO;
+
+        if (IntRect(EXIT_BUTTON_POS_X, EXIT_BUTTON_POS_Y,
+                    EXIT_BUTTON_WIDTH, EXIT_BUTTON_HEIGHT).contains(Mouse::getPosition(window)))
+            menuNum = EXIT;
+
+        if (IntRect(ABOUT_BUTTON_POS_X, ABOUT_BUTTON_POS_Y,
+                    ABOUT_BUTTON_WIDTH, ABOUT_BUTTON_HEIGHT).contains(Mouse::getPosition(window)))
+            menuNum = ABOUT;
+
+        if (IntRect(CHECKING_ODDS_BUTTON_POS_X, CHECKING_ODDS_BUTTON_POS_Y,
+                    CHECKING_ODDS_BUTTON_WIDTH, CHECKING_ODDS_BUTTON_HEIGHT).contains(Mouse::getPosition(window)))
+            menuNum = CHECKING_ODDS;
+
+
+        if (Mouse::isButtonPressed(Mouse::Left))
+        {
+            if (menuNum == START_INFO)
+                start_info_button_pressed(window);
+            else if (menuNum == EXIT)
+                exit_button_pressed(window);
+            else if (menuNum == ABOUT)
+                about_button_pressed(window);
+            else if (menuNum == CHECKING_ODDS)
+                checking_odds_button_pressed(window);
+        }
+    }
+}
--- menu.h
+++ menu.h
@@ -3,6 +3,94 @@
 //
 
 #ifndef MIPT_NAVIGATOR_MENU_H
+
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <iostream>
+
+using namespace sf;
+using std::string;
+
+class Menu
+{
+private:
+    const static int START_INFO = 1;
+    const static int EXIT = 2;
+    const static int ABOUT = 3;
+    const static int CHECKING_ODDS = 4;
+
+    const static int START_INFO_BUTTON_POS_X = 325;
+    const static int START_INFO_BUTTON_POS_Y = 100;
+    const static int START_INFO_BUTTON_WIDTH = 326;
+    const static int START_INFO_BUTTON_HEIGHT = 321;
+
+    const static int EXIT_BUTTON_POS_X = 1020;
+    const static int EXIT_BUTTON_POS_Y = 525;
+    const static int EXIT_BUTTON_WIDTH = 400;
+    const static int EXIT_BUTTON_HEIGHT = 500;
+
+    const static int ABOUT_BUTTON_POS_X = 400;
+    const static int ABOUT_BUTTON_POS_Y = 400;
+    const static int ABOUT_BUTTON_WIDTH = 400;
+    const static int ABOUT_BUTTON_HEIGHT = 400;
+
+    const static int CHECKING_ODDS_BUTTON_POS_X = 400;
+    const static int CHECKING_ODDS_BUTTON_POS_Y = 400;
+    const static int CHECKING_ODDS_BUTTON_WIDTH = 400;
+    const static int CHECKING_ODDS_BUTTON_HEIGHT = 400;
+
+    const static int WINDOW_POSITION_X = 0;
+    const static int WINDOW_POSITION_Y = 0;
+    const static int WINDOW_WIDTH = 1200 ;
+    const static int WINDOW_HEIGHT = 675;
+
+protected:
+    Image menu_background;
+    Texture menu_background_texture;
+    Sprite menu_background_sprite;
+
+    Image start_info_button;
+    Texture start_info_button_texture;
+    Sprite start_info_button_sprite;
+
+    Image exit_button;
+    Texture exit_button_texture;
+    Sprite exit_button_sprite;
+
+    Image checking_odds_button;
+    Texture checking_odds_texture;
+    Sprite checking_odds_sprite;
+
+    Image about_button;
+    Texture about_button_texture;
+    Sprite about_button_sprite;
+
+    int menuNum = 0;
+public:
+
+    //---------------------------------------creating sprites for menu:
+    void create_menu_background(const string& menu_background);
+    void create_start_info_button(const string& start_info_button_);
+    void create_checking_odds_button(const string& checking_odds_button_);
+    void create_about_button(const string& about_button_);
+    void create_exit_button(const string& exit_button_);
+    //------------------------------------------------------------------
+
+
+    //------------------------------------------------processing bottons:
+    void welcome_page(const string& menu_background_,
+                      const string& start_info_,
+                      const string& exit_button_,
+                      const string& about_button,
+                      const string& checking_odds_);                //menu
+
+    void start_info_button_pressed(RenderWindow& window);           //start info botton
+    static void exit_button_pressed(RenderWindow& window);          //exit botton
+    void checking_odds_button_pressed(RenderWindow& window);        //checking odds botton
+    void about_button_pressed(RenderWindow& window);                //information about authors
+    void processing_menu(RenderWindow& window);                     //processing keys
+    //------------------------------------------------------------------
+};
 #define MIPT_NAVIGATOR_MENU_H
 
 #endif //MIPT_NAVIGATOR_MENU_H
