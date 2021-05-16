a2470bde3460f1e0f5cba4e977ed875bf01c932d
TheRedHotHabanero <lkrn10001@gmail.com>
2021-05-16 15:50:17 +0300

    phystech school class updated with interface

--- phystech_school.h
+++ phystech_school.h
@@ -6,12 +6,34 @@
 
 class Phystech_School
 {
+private:
+    int number_of_subjects = 3;         //number of subjects for summ of points, change if necessary
+    bool computer_science == false;     // if this subject is in summ for points, set it true.
+    bool math = true;
+    bool physics = false;
+    bool russian_language = true;
+    bool biology = false;
+    bool chemistry = false;
 protected:
-//баллы егэ для поступления, создание спрайтов для всех кнопок и тд
+    //creating sprites for all buttons, etc. (like menu.h and menu.cpp)
 public:
-    //расчет поступления
-    //создание кнопок выхода, фичи, воспроизведение звука
-    //часть окна со скучной информацией
+    //calculation of receipts
+    virtual void counting_points(int number_of_subjects, int summ);
+
+    //creating exit button (like menu.h and menu.cpp)
+    virtual void create_exit_button(const string& exit_button_);
+
+    //creating a trick button (like menu.h and menu.cpp)
+    virtual void create_trick_button(const string& trick_button_);
+
+    //the first window of the Phystech School (like welcome_page from menu)
+    virtual void welcome_school_page();
+
+    //sound playback, but her znaet if I need a function to play sound
+    virtual void make_sound(const string& sound_)
+
+    //part of the window with boring information, but also her znaet if i need this function
+    virtual void show_school_information();
 
 };
 
