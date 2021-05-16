88c95357b4ea3b68ae9ab0af0004f50f5940ab47
TheRedHotHabanero <lkrn10001@gmail.com>
2021-05-16 15:32:07 +0300

    phystech school class started

--- phystech_school.h
+++ phystech_school.h
@@ -3,6 +3,18 @@
 //
 
 #ifndef MIPT_NAVIGATOR_PHYSTECH_SCHOOL_H
+
+class Phystech_School
+{
+protected:
+//баллы егэ для поступления, создание спрайтов для всех кнопок и тд
+public:
+    //расчет поступления
+    //создание кнопок выхода, фичи, воспроизведение звука
+    //часть окна со скучной информацией
+
+};
+
 #define MIPT_NAVIGATOR_PHYSTECH_SCHOOL_H
 
 #endif //MIPT_NAVIGATOR_PHYSTECH_SCHOOL_H
